79729306f929c4cab958f47bc0cfb919523b40b1
Tim Gee <writetotimgee@gmail.com>
2020-10-03 17:55:16 +0100

    Throw an error if the verifier did not complete in time

--- src/main/java/com/trickl/flux/websocket/VerifierComplete.java
+++ src/main/java/com/trickl/flux/websocket/VerifierComplete.java
@@ -1,5 +1,6 @@
 package com.trickl.flux.websocket;
 
+import com.trickl.exceptions.StepVerifierException;
 import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -17,17 +18,21 @@ public class VerifierComplete {
    * @param duration how long to wait
    */
   public void waitComplete(Duration duration) {
+    boolean didComplete = false;
     try {
-      completeSignal.await(duration.toMillis(), TimeUnit.MILLISECONDS);
+      didComplete = completeSignal.await(duration.toMillis(), TimeUnit.MILLISECONDS);
     } catch (InterruptedException ex) {
       log.warning("Verifier interrupted");
+    }
+    if (!didComplete) {
+      throw new StepVerifierException("Verifier did not complete within " + duration.toString());
     }
   }
 
   /**
    * Block until complete.
    */
-  public void waitComplete() {
+  public void waitComplete() {    
     try {
       completeSignal.await();
     } catch (InterruptedException ex) {
